95a1b891b578802f8cff85ac935e42d4a16affc4
pol <ypodim@gmail.com>
2024-05-30 23:36:02 -0700

    eye_sensor operational

--- src/eye_sensors.py
+++ src/eye_sensors.py
@@ -29,6 +29,7 @@ unset_pin.direction = digitalio.Direction.OUTPUT
 
 
 # os.system("play /usr/share/sounds/alsa/Noise.wav")
+options = dict(url="http://astrapi:8888/data/")
 
 class Relay():
     def __init__(self, set_pin, unset_pin, name="relay"):
@@ -52,109 +53,72 @@ class Relay():
             self.set_off()
         else:
             self.set_on()
+    def parse_action(self, action):
+        if action == "set":
+            self.set_on()
+        if action == "unset":
+            self.set_off()
+        if action == "toggle":
+            self.toggle()
 
 
-class Store(object):
-    def __init__(self):
-        self._store = dict(minutes=[], hours=[])
-        self.minutes = 60*24 # last 24h in minutes
-        self.hours = 24*365 # 1 year in hours
-        self.minutes_buffer = []
-    def load(self, filename):
-        try:
-            with open(filename, "r") as f:
-                raw_data = f.read()
-                self._store = json.loads(raw_data)
-        except FileNotFoundError:
-            print("sensor data file %s not found" % filename)
-    def save(self, filename):
-        with open(filename, "w+") as f:
-            raw_data = json.dumps(self._store)
-            f.write(raw_data)
-    def insert(self, value):
-        now = time.time()
-        self._store["minutes"].append((now, value))
-        if len(self._store["minutes"]) > self.minutes:
-            del self._store["minutes"][0]
-
-        self.minutes_buffer.append(value)
-        if len(self.minutes_buffer) >= 60:
-            hour_average = sum(self.minutes_buffer) / len(self.minutes_buffer)
-            self.minutes_buffer = []
-
-            self._store["hours"].append((now, hour_average))
-            if len(self._store["hours"]) > self.hours:
-                del self._store["hours"][0]
-    def get_last_value(self):
-        return self._store["minutes"][-1][1]
-    def get_minutes(self):
-        return self._store["minutes"]
-
-class Sensor(Store):
-    def __init__(self, name=""):
-        Store.__init__(self)
+class Sensor(object):
+    def __init__(self, name="", poll_interval=1, send_interval=60, calc_avg=True):
         self.name = name
-        self.instant_value = 0
         self.instant_values_sum = 0
         self.instant_values_tot = 0
-        self.last_datapoint = 0
-        self.datapoint_interval = 60 #seconds
-        self.filename = "%s.json" % name
-        self.load(self.filename)
-    
-    def get_measurement(self):
-        raise NotImplementedError
-    def update(self):
-        self.get_measurement()
-        self.instant_values_sum += self.instant_value
-        self.instant_values_tot += 1
+        self.last_send = 0
+        self.send_interval = send_interval #seconds
+        self.poll_interval = poll_interval #seconds
+        self.calc_avg = calc_avg
+        self.max_val = 0
 
-        now = time.time()
-        if now - self.last_datapoint > self.datapoint_interval:
-            if self.instant_values_tot > 0:
-                value = self.instant_values_sum/self.instant_values_tot
-                self.instant_values_sum = 0
-                self.instant_values_tot = 0
-
-            self.insert(value)
-            self.last_datapoint = now
-
-        # future = asyncio.create_task(self.update())
-
-    def get_stats(self):
-        stats = {}
-        stats["store_size"] = len(self.get_minutes())
-        stats["inst_total"] = self.instant_values_tot
-        stats["inst_sum"] = self.instant_values_sum
-        return stats
-        
     def send(self, sensor_name, sensor_value):
-        print(sensor_name, sensor_value)
-        return
         data = dict(
             sensor_name=sensor_name, 
-            sensor_value=sensor_value, 
-            # sensor_stats=s.get_stats(),
-            tstamp=time.time())
+            sensor_value=sensor_value)
 
         datastr = json.dumps(data)
-        url = "http://astrapi:8888/data/"
         try:
-            requests.put(url, params=dict(datastr=datastr))
-            print("success!")
+            r = requests.post(options.get("url"), params=dict(datastr=datastr))
         except:
             print("error connecting")
             data = dict(
                 sensor_name="error_connecting", 
-                sensor_value=1, 
-                tstamp=time.time())
+                sensor_value=1)
          
+    def add_value(self, val):
+        self.max_val = max(val, self.max_val)
+        self.instant_values_sum += val
+        self.instant_values_tot += 1
+    def get_value(self):
+        value = None
+
+        if self.calc_avg:
+            value = self.instant_values_sum/self.instant_values_tot
+        else:
+            value = self.max_val
+            self.max_val = 0
+
+        self.instant_values_sum = 0
+        self.instant_values_tot = 0
+        return value
+
     def run(self):
         self.running = 1
         while self.running:
             val = self.read_value()
-            self.send(self.name, val)
-            time.sleep(1)
+            self.add_value(val)
+
+            now = time.time()
+            if now - self.last_send > self.send_interval:
+                value = self.get_value()
+
+                self.send(self.name, value)
+                self.last_send = now
+
+            time.sleep(self.poll_interval)
+
     def stop(self):
         self.running = 0
 
@@ -192,25 +156,21 @@ class Light(Sensor):
         super(Light, self).__init__(name="light")
         self.pin = AnalogIn(adc, pin)
     def read_value(self):
-        max_value = 65536
-        # self.instant_value = 100*self.pin.value/max_value
-        self.instant_value = self.pin.value
-        # self.send("light2", self.pin.value)
         return self.pin.value
 
 
 class Microphone(Sensor):
     def __init__(self, mcp, pin):
-        super(Microphone, self).__init__(name="mic")
+        super(Microphone, self).__init__(name="mic", poll_interval=0.1, calc_avg=False)
         self.pin = AnalogIn(mcp, pin)
         self.past_values = []
         self.past_values_size = 10
-    def get_measurement(self):
-        val = 100.0*self.pin.value/65536
-        self.past_values.append(val)
+    def read_value(self):
+        self.past_values.append(self.pin.value)
+        ret_val = max(self.past_values) - min(self.past_values)
         if len(self.past_values) > self.past_values_size:
             del self.past_values[0]
-        self.instant_value = max(self.past_values) - min(self.past_values)
+        return ret_val
 
 class SoilConductivity(Sensor):
     def __init__(self):
@@ -233,10 +193,10 @@ class InternalTemperature(Sensor):
     def __init__(self, i2c):
         super(InternalTemperature, self).__init__(name="internaltemp")
         self.i2c = i2c
-    def get_measurement(self):
-        t = adafruit_mcp9808.MCP9808(i2c)
+    def read_value(self):
+        t = adafruit_mcp9808.MCP9808(self.i2c)
         temp_f = t.temperature * 9.0 / 5.0 + 32.0
-        self.instant_value = temp_f
+        return temp_f
 
 class TestSensor(Sensor):
     def __init__(self):
@@ -244,94 +204,71 @@ class TestSensor(Sensor):
     def get_measurement(self):
         self.instant_value = random.random() * 100
 
-
-def date_cmp(pair, threshold):
-    date = pair[0]
-    return time.time() - date < threshold
-    # return datetime.datetime.now() - date < datetime.timedelta(seconds=threshold)
-
 class Manager(object):
     def __init__(self):
+        self.i2c = board.I2C()
         self.sensors = []
-        self.retain_threshold = 60*60*24 #seconds
-        # self.feeder = Feeder(actions_clb=self.action)
-        # self.buffer = []
-    def set_sensors(self, sensors):
-        self.sensors = sensors
-    def action(self, action_name, action_value):
-        print("action:", action_name, action_value)
-    def get_sensor_names(self):
-        return [sensor.name for sensor in self.sensors]
-    def get_data(self, sensor_name):
-        for sensor in self.sensors:
-            if sensor.name == sensor_name:
-                func = lambda datalist: date_cmp(datalist, self.retain_threshold)
-                date_tranform = lambda x: (datetime.datetime.fromtimestamp(x[0]), x[1])
-                result = list(filter(func, sensor.get_minutes()))
-                result = list(map(date_tranform, result))
-                return result
-        return (0,0)
+        self.actuators = dict(relay=Relay(set_pin, unset_pin))
+    def setup(self):
+        LIGHT_PIN = 0
+        LIGHT_PIN2 = 2
+        MIC_PIN = 1
 
-    def save_to_file(self):
-        for sensor in self.sensors:
-            sensor.save(sensor.filename)
-        # asyncio.get_event_loop().call_later(30, self.save_to_file)
+        adc = ADC.ADS7830(self.i2c)
 
-    def stop(self):
+        light2 = Light(adc, LIGHT_PIN2)
+        threading.Thread(target=light2.run).start()
+        self.sensors.append(light2)
+
+        mic = Microphone(adc, MIC_PIN)
+        threading.Thread(target=mic.run).start()
+        self.sensors.append(mic)
+
+        internalTemp = InternalTemperature(self.i2c)
+        threading.Thread(target=internalTemp.run).start()
+        self.sensors.append(internalTemp)
+
+        soil_conductivity = SoilConductivity()
+        threading.Thread(target=soil_conductivity.run).start()
+        self.sensors.append(soil_conductivity)
+
+        temperature = Temperature()
+        threading.Thread(target=temperature.run).start()
+        self.sensors.append(temperature)
+    def loop_once(self):
+        
+        response = None
+        try:
+            r = requests.get(options.get("url"))
+        except:
+            print("error connecting to:", options.get("url"))
+            return
+
+        if r.text:
+            try:
+                response = json.loads(r.text)
+            except:
+                return
+            if response and "action" in response:
+                action = response["action"]
+                actuator = response["actuator"]
+                if actuator == "relay":
+                    self.actuators["relay"].parse_action(action)
+    def run(self):
+        while 1:
+            time.sleep(1)
+            self.loop_once()
+    def cleanup(self):
+        print("time to die")
+        self.i2c.deinit()
         for s in self.sensors:
             s.stop()
 
 
-
 if __name__ == "__main__":
     manager = Manager()
+    manager.setup()
     try:
-        with board.I2C() as i2c:
-            LIGHT_PIN = 0
-            LIGHT_PIN2 = 2
-            MIC_PIN = 1
-            sensors = []
-
-            adc = ADC.ADS7830(i2c)
-
-            # light = Light(adc, LIGHT_PIN)
-            # light.update()
-            # sensors.append(light)
-
-            light2 = Light(adc, LIGHT_PIN2)
-            threading.Thread(target=light2.run).start()
-            sensors.append(light2)
-
-            # mic = Microphone(adc, MIC_PIN)
-            # mic.update()
-            # sensors.append(mic)
-
-            # internalTemp = InternalTemperature(i2c)
-            # internalTemp.update()
-            # sensors.append(internalTemp)
-
-            soil_conductivity = SoilConductivity()
-            threading.Thread(target=soil_conductivity.run).start()
-            sensors.append(soil_conductivity)
-
-            # temp = Temperature()
-            # temp.update()
-            # sensors.append(temp)
-            temperature = Temperature()
-            threading.Thread(target=temperature.run).start()
-            sensors.append(temperature)
-
-            # sensor = TestSensor()
-            # await sensor.update()
-            # sensors.append(sensor)
-            manager.set_sensors(sensors)
-            
-            while 1:
-                time.sleep(1)
-
+        manager.run()
     except KeyboardInterrupt:
-        print("time to die")
-        manager.stop()
-
-
-
+        manager.cleanup()
